314240d520b8925d6f2c2d2b3d0654f992645529
e9x <x@sys32.dev>
2022-03-13 16:02:18 -0400

    javascript obfuscator

--- craco.config.js
+++ craco.config.js
@@ -3,12 +3,38 @@ const { when, whenDev, whenProd } = require('@craco/craco');
 
 module.exports = {
 	webpack: {
+		...whenProd(() => ({
+			devtool: 'none',
+		}), {}),
 		plugins: [
 			...whenProd(() => [ 
 				new WebpackObfuscator({
+					compact: true,
+					controlFlowFlattening: false,
+					deadCodeInjection: false,
+					debugProtection: false,
+					debugProtectionInterval: 0,
+					disableConsoleOutput: false,
+					identifierNamesGenerator: 'hexadecimal',
+					log: false,
+					numbersToExpressions: false,
+					renameGlobals: false,
+					selfDefending: false,
+					simplify: true,
+					splitStrings: false,
 					stringArray: true,
-					stringArrayWrappersCount: 10,
+					stringArrayCallsTransform: false,
+					stringArrayCallsTransformThreshold: 0.5,
+					stringArrayEncoding: ['rc4'],
+					stringArrayIndexShift: true,
+					stringArrayRotate: true,
+					stringArrayShuffle: true,
+					stringArrayWrappersCount: 1,
+					stringArrayWrappersChainedCalls: true,
+					stringArrayWrappersParametersMaxCount: 2,
+					stringArrayWrappersType: 'variable',
 					stringArrayThreshold: 1,
+					unicodeEscapeSequence: false
 				}),
 			], []),
 		],
