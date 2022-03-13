ebad24fb2ee0acc917ef360988cf7a1043ffcfe4
e9x <x@sys32.dev>
2022-03-12 20:02:07 -0500

    more

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,18 @@
-import { Component } from 'react';
+import React, { Component, createElement, createRef } from 'react';
 import root from '../root.js';
+import TypeWriter from '../TypeWriter.js';
 
 export default class Home extends Component {
-	render(){
+	render() {
 		root.dataset.page = 'home';
-		
+
 		return (
 			<main>
-				<p>Hello, World!</p>
+				<h1 className='main'>
+					Your internet<br/>
+					<TypeWriter element={<h1 className='adjective'>ae</h1>} speed={50} delay={5000} strings={['More Secure.', 'More Private.', 'Uncensored.', 'Safer.', 'Easier', 'More Fun.']}></TypeWriter><br/>
+				</h1>
+				<div className='description'>SystemYA is a proxy site capable of accessing .</div>
 			</main>
 		);
 	}
---
+++ src/TypeWriter.js
@@ -0,0 +1,34 @@
+import { Component, createRef } from 'react';
+import SleepingComponent from './SleepingComponent.js';
+
+export default class TypeWriter extends SleepingComponent {
+	element = createRef();
+	async componentDidMount(){
+		const { speed, delay, strings } = this.props;
+		
+		const { current: element } = this.element;
+	
+		element.textContent = strings[0];
+		
+		this.mounted = true;
+
+		while(!this.unmounting){
+			for(let string of strings){
+				while(element.textContent.length){
+					element.textContent = element.textContent.slice(0, -1);
+					await this.sleep(speed);
+				}
+
+				for(let i = 0; i < string.length; i++){
+					element.textContent += string[i];
+					await this.sleep(speed, this);
+				}
+
+				await this.sleep(delay);
+			}
+		}
+	}
+	render(){
+		return <span ref={this.element}></span>
+	}
+};
