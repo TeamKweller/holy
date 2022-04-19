ea83392aa519928e7ac387081200cac61c66a6f8
David Reed <x@sys32.dev>
2022-04-19 14:41:21 -0400

    make thumbnail script more verbose

--- compile-thumbnails.sh
+++ compile-thumbnails.sh
@@ -1,11 +1,14 @@
+echo You need ImageMagick installed and added to your path to use this script.
+
 categories=( "social" "html5" "ports" "gba" "genesis" "nes" )
 
-mkdir ./src/assets/thumbnails/
+mkdir ./src/assets/thumbnails/ 2> /dev/null
 
 cd game-thumbnails
 	for category in "${categories[@]}"
 	do
 		cd $category
+			echo Converting * in $category
 			convert +append ./* ../../src/assets/thumbnails/$category.png
 		cd ..
 	done
