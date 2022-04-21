ed7379bd1cda8c95283d9a3e27789f022d50c8f3
David Reed <x@sys32.dev>
2022-04-20 23:37:30 -0400

    prepare for games cdn

--- src/root.js
+++ src/root.js
@@ -4,14 +4,17 @@ document.title = 'Holy Unblocker';
 
 let _bareCDN;
 let _rhApp;
+let _gamesCDN;
 
 if (process.env.NODE_ENV === 'development') {
 	_bareCDN = 'http://localhost:8001/';
 	_rhApp = 'http://localhost:8002/';
+	_gamesCDN = 'http://localhost:3000/';
 } else {
 	const { host } = global.location;
 	_bareCDN = `https://uv.${host}/bare/`;
 	_rhApp = `https://rh.${host}/`;
+	_gamesCDN = `https://gs.${host}/`;
 }
 
 export const bareCDN = _bareCDN;
