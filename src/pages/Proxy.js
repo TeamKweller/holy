1e07604fe20f531db72368bd45a059c8b9dc8025
David Reed <x@sys32.dev>
2022-05-15 00:42:06 -0400

    proxy function

--- src/pages/Proxy.js
+++ src/pages/Proxy.js
@@ -1,6 +1,6 @@
 import { Obfuscated, ObfuscatedA } from '../obfuscate.js';
 import { PATREON_URL, TN_DISCORD_URL } from '../root.js';
-import { Component, createRef } from 'react';
+import { createRef, useEffect, useRef, useState } from 'react';
 import { NorthWest, Search } from '@mui/icons-material';
 import Footer from '../Footer.js';
 import ServiceFrame from '../ServiceFrame.js';
@@ -10,240 +10,227 @@ import clsx from 'clsx';
 import '../styles/Proxy.scss';
 import { ThemeInputBar } from '../ThemeElements.js';
 
-export default class Proxies extends Component {
-	service_frame = createRef();
-	input = createRef();
-	form = createRef();
-	suggested = createRef();
-	state = {
-		omnibox_entries: [],
-		input_focused: false,
-	};
-	/**
-	 * @returns {import('react').RefObject<import('../MainLayout.js').default>}
-	 */
-	get layout() {
-		return this.props.layout;
-	}
-	componentDidMount() {
-		this.on_input();
-	}
-	async on_input() {
-		if (this.input_value !== this.input.current.value) {
-			this.input_value = this.input.current.value;
+export default function Proxies(props) {
+	const service_frame = useRef();
+	const input = useRef();
+	const form = useRef();
+	const suggested = useRef();
+	const input_value = useRef();
+	const last_input = useRef();
+	const [last_select, set_last_select] = useState(-1);
+	const [omnibox_entries, set_omnibox_entries] = useState([]);
+	const [input_focused, set_input_focused] = useState(false);
 
-			this.setState({
-				omnibox_entries: await this.service_frame.current.omnibox_entries(
-					this.input.current.value
-				),
-			});
+	async function on_input() {
+		if (input_value.current !== input.current.value) {
+			input_value.current = input.current.value;
+
+			set_omnibox_entries(
+				await service_frame.current.omnibox_entries(input.current.value)
+			);
 		}
 	}
-	search_submit() {
+
+	function search_submit() {
 		let value;
 
-		if (this.state.last_select === -1 || this.last_input === 'input') {
-			value = this.input.current.value;
+		if (last_select === -1 || last_input.current === 'input') {
+			value = input.current.value;
 		} else {
-			value = textContent(this.state.omnibox_entries[this.state.last_select]);
-			this.input.current.value = value;
+			value = textContent(omnibox_entries[last_select]);
+			input.current.value = value;
 		}
 
-		this.setState({ input_focused: false });
-		this.service_frame.current.proxy(this.input.current.value);
-		this.on_input();
+		set_input_focused(false);
+		service_frame.current.proxy(input.current.value);
+		on_input();
 	}
-	render() {
-		const render_suggested =
-			this.state.input_focused && this.state.omnibox_entries.length !== 0;
-		const suggested = [];
 
-		if (render_suggested) {
-			for (let i = 0; i < this.state.omnibox_entries.length; i++) {
-				const text = createRef();
+	useEffect(() => {
+		on_input();
+	});
 
-				suggested.push(
-					<div
-						key={i}
-						tabIndex={0}
-						className={clsx('option', i === this.state.last_select && 'hover')}
-						onClick={() => {
-							this.last_input = 'select';
-							this.input.current.value = text.current.textContent;
-							this.search_submit();
+	const render_suggested = input_focused && omnibox_entries.length !== 0;
+	const suggested_list = [];
+
+	if (render_suggested) {
+		for (let i = 0; i < omnibox_entries.length; i++) {
+			const text = createRef();
+
+			suggested_list.push(
+				<div
+					key={i}
+					tabIndex={0}
+					className={clsx('option', i === last_select && 'hover')}
+					onClick={() => {
+						last_input.current = 'select';
+						input.current.value = text.current.textContent;
+						search_submit();
+					}}
+					onMouseOver={() => {
+						set_last_select(i);
+					}}
+				>
+					<Search className="search" />
+					<span
+						className="text"
+						ref={text}
+						dangerouslySetInnerHTML={{
+							__html: omnibox_entries[i],
 						}}
-						onMouseOver={() => {
+					/>
+					<NorthWest className="open" />
+				</div>
+			);
+		}
+	}
+
+	let engine;
+	const format = props.layout.current.settings.get('search');
+
+	for (let _engine of engines) {
+		if (_engine.format === format) {
+			engine = _engine;
+			break;
+		}
+	}
+
+	if (engine === undefined) {
+		engine = engines[0];
+	}
+
+	return (
+		<>
+			<main className="proxy">
+				<ServiceFrame layout={props.layout} ref={service_frame} />
+				<form
+					className="omnibox"
+					data-suggested={Number(render_suggested)}
+					data-focused={Number(input_focused)}
+					onSubmit={event => {
+						event.preventDefault();
+						search_submit();
+					}}
+					onBlur={event => {
+						if (!form.current.contains(event.relatedTarget)) {
+							set_input_focused(false);
+						}
+					}}
+					ref={form}
+				>
+					<ThemeInputBar>
+						<Search className="icon" />
+						<input
+							type="text"
+							placeholder={`Search ${engine.name} or type a URL`}
+							required={last_select === -1}
+							autoComplete="off"
+							className="thin-pad-left"
+							list="proxy-omnibox"
+							ref={input}
+							onInput={on_input}
+							onFocus={() => {
+								on_input();
+								set_input_focused(true);
+								set_last_select(-1);
+							}}
+							onClick={() => {
+								on_input();
+								set_input_focused(true);
+								set_last_select(-1);
+							}}
+							onChange={() => {
+								last_input.current = 'input';
+								set_last_select(-1);
+							}}
+							onKeyDown={event => {
+								let prevent_default = true;
+
+								switch (event.code) {
+									case 'Escape':
+										this.setState({
+											input_focused: false,
+										});
+										break;
+									case 'ArrowDown':
+									case 'ArrowUp':
+										{
+											let last_i = last_select;
+
+											let next;
+
+											switch (event.code) {
+												case 'ArrowDown':
+													if (last_i >= omnibox_entries.length - 1) {
+														next = 0;
+													} else {
+														next = last_i + 1;
+													}
+													break;
+												case 'ArrowUp':
+													if (last_i <= 0) {
+														next = omnibox_entries.length - 1;
+													} else {
+														next = last_i - 1;
+													}
+													break;
+												case 'Enter':
+													search_submit();
+													break;
+												// no default
+											}
+
+											last_input.current = 'select';
+
+											set_last_select(next);
+										}
+										break;
+									default:
+										prevent_default = false;
+										break;
+									// no default
+								}
+
+								if (prevent_default) {
+									event.preventDefault();
+								}
+							}}
+						/>
+					</ThemeInputBar>
+					<div
+						ref={suggested}
+						className="suggested"
+						onMouseLeave={() => {
 							this.setState({
-								last_select: i,
+								last_select: -1,
 							});
 						}}
 					>
-						<Search className="search" />
-						<span
-							className="text"
-							ref={text}
-							dangerouslySetInnerHTML={{
-								__html: this.state.omnibox_entries[i],
-							}}
-						/>
-						<NorthWest className="open" />
+						{suggested_list}
 					</div>
-				);
-			}
-		}
-
-		let engine;
-		const format = this.props.layout.current.settings.get('search');
-
-		for (let _engine of engines) {
-			if (_engine.format === format) {
-				engine = _engine;
-				break;
-			}
-		}
-
-		if (engine === undefined) {
-			engine = engines[0];
-		}
-
-		return (
-			<>
-				<main className="proxy">
-					<ServiceFrame layout={this.layout} ref={this.service_frame} />
-					<form
-						className="omnibox"
-						data-suggested={Number(render_suggested)}
-						data-focused={Number(this.state.input_focused)}
-						onSubmit={event => {
-							event.preventDefault();
-							this.search_submit();
-						}}
-						onBlur={event => {
-							if (!this.form.current.contains(event.relatedTarget)) {
-								this.setState({ input_focused: false });
-							}
-						}}
-						ref={this.form}
-					>
-						<ThemeInputBar>
-							<Search className="icon" />
-							<input
-								type="text"
-								placeholder={`Search ${engine.name} or type a URL`}
-								required={this.state.last_select === -1}
-								autoComplete="off"
-								className="thin-pad-left"
-								list="proxy-omnibox"
-								ref={this.input}
-								onInput={this.on_input.bind(this)}
-								onFocus={() => {
-									this.on_input();
-									this.setState({ input_focused: true, last_select: -1 });
-								}}
-								onClick={() => {
-									this.on_input();
-									this.setState({ input_focused: true, last_select: -1 });
-								}}
-								onChange={() => {
-									this.last_input = 'input';
-									this.setState({
-										last_select: -1,
-									});
-								}}
-								onKeyDown={event => {
-									let prevent_default = true;
-
-									switch (event.code) {
-										case 'Escape':
-											this.setState({
-												input_focused: false,
-											});
-											break;
-										case 'ArrowDown':
-										case 'ArrowUp':
-											{
-												let last_i = this.state.last_select;
-
-												let next;
-
-												switch (event.code) {
-													case 'ArrowDown':
-														if (
-															last_i >=
-															this.state.omnibox_entries.length - 1
-														) {
-															next = 0;
-														} else {
-															next = last_i + 1;
-														}
-														break;
-													case 'ArrowUp':
-														if (last_i <= 0) {
-															next = this.state.omnibox_entries.length - 1;
-														} else {
-															next = last_i - 1;
-														}
-														break;
-													case 'Enter':
-														this.search_submit();
-														break;
-													// no default
-												}
-
-												this.last_input = 'select';
-
-												this.setState({
-													last_select: next,
-												});
-											}
-											break;
-										default:
-											prevent_default = false;
-											break;
-										// no default
-									}
-
-									if (prevent_default) {
-										event.preventDefault();
-									}
-								}}
-							/>
-						</ThemeInputBar>
-						<div
-							ref={this.suggested}
-							className="suggested"
-							onMouseLeave={() => {
-								this.setState({
-									last_select: -1,
-								});
-							}}
-						>
-							{suggested}
-						</div>
-					</form>
-					<p>
-						<Obfuscated>
-							This is a free service paid for by our Patreons. If you want
-							faster servers, donate to Holy Unblocker on{' '}
-						</Obfuscated>
-						<ObfuscatedA className="theme-link" href={PATREON_URL}>
-							<Obfuscated>Patreon</Obfuscated>
-						</ObfuscatedA>
-						.
-					</p>
-					<p>
-						<Obfuscated>Click </Obfuscated>
-						<ObfuscatedA className="theme-link" href={TN_DISCORD_URL}>
-							here
-						</ObfuscatedA>
-						<Obfuscated>
-							{' '}
-							to join the TN Discord for access to new Holy Unblocker links.
-						</Obfuscated>
-					</p>
-				</main>
-				<Footer />
-			</>
-		);
-	}
+				</form>
+				<p>
+					<Obfuscated>
+						This is a free service paid for by our Patreons. If you want faster
+						servers, donate to Holy Unblocker on{' '}
+					</Obfuscated>
+					<ObfuscatedA className="theme-link" href={PATREON_URL}>
+						<Obfuscated>Patreon</Obfuscated>
+					</ObfuscatedA>
+					.
+				</p>
+				<p>
+					<Obfuscated>Click </Obfuscated>
+					<ObfuscatedA className="theme-link" href={TN_DISCORD_URL}>
+						here
+					</ObfuscatedA>
+					<Obfuscated>
+						{' '}
+						to join the TN Discord for access to new Holy Unblocker links.
+					</Obfuscated>
+				</p>
+			</main>
+			<Footer />
+		</>
+	);
 }
