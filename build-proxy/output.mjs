4633e492e672e6b6b1c1891f0128df42f9459d1b
David Reed <x@sys32.dev>
2022-06-06 16:55:15 -0400

    move build-proxy scripts to scripts, make proxy build scripts cjs

--- build-proxy/output.mjs
+++
@@ -1,7 +0,0 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export const WEBROOT = join(__dirname, '..', 'public');
